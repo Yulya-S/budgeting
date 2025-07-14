f912bb9d2809bdc28399f397461848595508db5f
Yulya-S <yulya_Shabsnova_03@mail.ru>
2025-07-14 08:16:32 +0800

    Dynamic page filling

--- budgeting/scripts/pages/wallet.gd
+++ budgeting/scripts/pages/wallet.gd
@@ -1,21 +1,30 @@
-extends Node2D
-@onready var Wallets = $Background/ScrollContainer/VBoxContainer
+extends Control
+# Подключение путей к объектам в сцене
+@onready var Wallets = $ScrollContainer/VBoxContainer
+
+# Путь к подгружаемой сцене транзакции
 var wallet_path = load("res://scenes/fragments/wallet.tscn")
+
+# Переменные
+var lines: Array = [] # Список объектов для создания на странице
 
 # Подключение сигналов
 func _ready() -> void:
 	Global.connect("update_page", Callable(self, "_update_page"))
 	_update_page()
 
+# Динамическое заполнение страницы
+func _process(delta: float) -> void:
+	if len(lines) > 0:
+		Wallets.add_child(wallet_path.instantiate())
+		Wallets.get_child(-1).set_values(lines.pop_front())
+
 # Заполнение страницы
-func _update_page():
+func _update_page() -> void:
 	for i in Wallets.get_children():
 		i.queue_free()
 		Wallets.remove_child(i)
-	var wallets_array: Array = Request.get_wallets()
-	for i in wallets_array:
-		Wallets.add_child(wallet_path.instantiate())
-		Wallets.get_child(-1).set_values(i)
+	lines = Request.get_wallets()
 
 # Обработка нажатия кнопки создания нового счета
 func _on_add_wallet_button_down() -> void: Global.emit_signal("open_window", Global.Pages.WALLET)
--- budgeting/scripts/pages/wallet_inf.gd
+++ budgeting/scripts/pages/wallet_inf.gd
@@ -1,31 +1,35 @@
-extends Node2D
+extends Control
 # Подключение путей к объектам в сцене
-@onready var Title = $Background/Wallet/Title
-@onready var Value = $Background/Wallet/Value
-@onready var Objects = $Background/ScrollContainer/VBoxContainer
-@onready var TotalCount = $Background/Total/Count
-@onready var TotalValue = $Background/Total/Value
+@onready var Title = $Wallet/Title
+@onready var Value = $Wallet/Value
+@onready var Objects = $ScrollContainer/VBoxContainer
+@onready var TotalCount = $Total/Count
+@onready var TotalValue = $Total/Value
 
 # Путь к подгружаемой сцене транзакции
 var cash_flow_path = load("res://scenes/fragments/cash_flow.tscn")
 
-var id = 1 # Индекс счета
+# Переменные
+var id = null # Индекс счета
+var lines: Array = [] # Список объектов для создания на странице
 
-# Заполнение данных
-func _ready() -> void:
+# Динамическое заполнение страницы
+func _process(delta: float) -> void:
+	if len(lines) > 0:
+		Objects.add_child(cash_flow_path.instantiate())
+		var obj: Dictionary = lines.pop_front()
+		Objects.get_child(-1).set_values(obj)
+		TotalValue.set_text(str(float(TotalValue.get_text()) + obj.value))
+		TotalCount.set_text(str(int(TotalCount.get_text()) + 1))	
+
+# Смена индекса объекта
+func set_object(obj_id: int) -> void:
+	id = obj_id
+	_update_values()
+
+# Заполнение данных на странице
+func _update_values() -> void:
 	var wallet_value: Dictionary = Request.select(Request.Tables.WALLETS, "*", "id="+str(id))[0]
-	var cash_flow_value: Array = Request.select_cash_flow_sum(id)
+	lines = Request.select_cash_flow_sum(id)
 	Title.set_text(wallet_value.title)
 	Value.set_text(str(wallet_value.value))
-	var sum: float = 0.0
-	var count: int = 0
-	for i in cash_flow_value:
-		Objects.add_child(cash_flow_path.instantiate())
-		Objects.get_child(-1).set_values(i)
-		sum += i.value
-	TotalCount.set_text(str(len(cash_flow_value)))
-	TotalValue.set_text(str(sum))
-	
-	
-	
-	
