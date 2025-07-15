04ce6178182c505705c60dc90b0b8dc4c227e635
Yulya-S <yulya_Shabsnova_03@mail.ru>
2025-07-15 11:12:54 +0800

    Generic Change Page Class

---
+++ budgeting/scripts/windows/creation_page.gd
@@ -0,0 +1,38 @@
+extends Control
+class_name CreationPage
+@onready var Error = $Error
+@onready var Delete = $Delete
+
+# Переменные
+var id = null # Индекс изменяемого объекта
+var table: Request.Tables = Request.Tables.WALLETS # Связанная таблица
+
+# Изменение информации о счете
+func _get_obj_data(obj_id: int, new_table: Request.Tables) -> Array:
+	table = new_table
+	id = obj_id
+	Delete.visible = id != null
+	return Request.select(table, "*", "id="+str(obj_id))
+
+# Проверка существуют ли подобные записи
+func _set_error(Error: Label, values: Array) -> bool:
+	if len(values) == 0: return Error.visible
+	elif not id: Global.set_error(Error, "Объект уже существует")
+	else: for i in values: if id != i.id:  Global.set_error(Error, "Объект уже существует")
+	return Error.visible
+
+# Обработка нажатия кнопки отмены
+func _on_close_button_down() -> void:
+	self.queue_free()
+	get_parent().remove_child(self)
+
+# Применение изменений
+func _apply_changes() -> void:
+	Global.emit_signal("update_page")
+	_on_close_button_down()	
+
+# Создание или изменение объекта
+func _create_update(values: Array) -> void:
+	if id: Request.update_record(table, id, values)
+	else: Request.insert_record(table, values)
+	_apply_changes()
--- budgeting/scripts/windows/wallet.gd
+++ budgeting/scripts/windows/wallet.gd
@@ -1,33 +1,23 @@
-extends Control
+extends CreationPage
 # Подключение путей к объектам в сцене
-@onready var Error = $Error
 @onready var Title = $Title
 @onready var Value = $Value
-@onready var Delete = $Delete
-
-var id = null # id изменяемого счета
 
 # Изменение информации о счете
-func set_object(obj_id: int) -> void:
-	var value = Request.select(Request.Tables.WALLETS, "*", "id="+str(obj_id))
-	if len(value) == 0: return
-	id = obj_id
+func set_object(obj_id: int, table: Request.Tables) -> void:
+	var value: Array = _get_obj_data(obj_id, table)
+	if len(value) < 0: return
 	Value.editable = id == null
-	Delete.visible = id != null
 	Title.set_text(value[0].title)
 	Value.set_text(str(value[0].value))
 
 # Проверка введенных данных
 func check_title() -> bool:
 	Error.visible = false
-	var values = Request.select(Request.Tables.WALLETS, "id", 'title="'+Title.get_text()+'"')
+	var values = Request.select(table, "id", 'title="'+Title.get_text()+'"')
 	# Проверка заполнености полей
 	if Title.get_text() == "": Global.set_error(Error, "Поле названия не должно быть пустым")
-	# Проверка существуют ли подобные записи
-	if len(values) == 0: return Error.visible
-	elif not id: Global.set_error(Error, "Счет с введенным именем уже существует")
-	else: for i in values: if id != i.id:  Global.set_error(Error, "Счет с введенным именем уже существует")
-	return Error.visible
+	return _set_error(Error, values)
 
 # Изменение значения названия кошелька
 func _on_title_text_changed() -> void:
@@ -37,22 +27,11 @@ func _on_title_text_changed() -> void:
 # Изменение значения счета
 func _on_value_text_changed() -> void: Global.text_changed_TextEdit(Value, true)
 
-# Обработка нажатия кнопки отмены
-func _on_close_button_down() -> void:
-	self.queue_free()
-	get_parent().remove_child(self)
-	
-func _apply_changes():
-	Global.emit_signal("update_page")
-	_on_close_button_down()
-
 # Обработка нажатия кнопки удаления счета
 func _on_delete_button_down() -> void:
-	Request.delete(Request.Tables.WALLETS, id)
+	Request.delete(table, id)
 	_apply_changes()
 
 # Обработка нажатия кнопки сохранения счета
 func _on_apply_button_down() -> void:
-	if id: Request.update_record(Request.Tables.WALLETS, id, ['"'+Title.get_text()+'"', float(Value.get_text())])
-	else: Request.insert_record(Request.Tables.WALLETS, ['"'+Title.get_text()+'"', float(Value.get_text())])
-	_apply_changes()
+	_create_update(['"'+Title.get_text()+'"', float(Value.get_text())])
