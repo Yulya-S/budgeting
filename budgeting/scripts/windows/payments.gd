04b1f41bd8b917375e50525f0a8a4ae1118170b7
Yulya-S <yulya_Shabsnova_03@mail.ru>
2025-07-20 07:56:37 +0800

    creating loan scenes

---
+++ budgeting/scripts/windows/payments.gd
@@ -0,0 +1,32 @@
+extends Movement
+# Подключение путей к объектам в сцене
+@onready var Total = $Extra/Total
+
+# Создание сцены
+func _ready() -> void:
+	Global.fill_optionButton(Wallet, Request.select(Request.Tables.WALLETS))
+	Global.fill_optionButton(Extra, Request.select(second_table, "*", "total>0"))
+	set_wallet()
+	set_extra()
+
+# Изменение раздела расхода
+func set_extra(extra_idx: int = 0) -> void:
+	Extra.selected = extra_idx
+	Total.set_text(str(Request.select(second_table, "total", "id="+str(Global.get_OB_id(Extra)))[0].total))
+	
+# Проведение дополнительных проверок на верность данных
+func _extra_errors() -> bool:
+	if float(Total) < float(Value.get_text()): Global.set_error(Error, "Нельзя перевысить значение суммы кредита")
+	return Error.visible
+
+# Изменение значение счета после проведения транзакции
+func _update_wallet_value(delete: bool = false) -> void:
+	var income = -1
+	if delete: income = 1
+	Request.update(Request.Tables.WALLETS, "value=value+"+str(income*float(Value.get_text())), "id="+str(Global.get_OB_id(Wallet)))
+	Request.update(Request.Tables.WALLETS, "total=total+"+str(income*float(Value.get_text())), "id="+str(Global.get_OB_id(Extra)))
+
+# Создание или изменение объекта
+func _create_update(values: Array) -> void:
+	values[1] = 2
+	super._create_update(values)
