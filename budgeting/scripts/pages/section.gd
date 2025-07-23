1576595ced45264793224364bec20b76b590297f
Yulya-S <yulya_Shabsnova_03@mail.ru>
2025-07-23 15:10:38 +0800

    window opening

--- budgeting/scripts/pages/section.gd
+++ budgeting/scripts/pages/section.gd
@@ -3,7 +3,7 @@ extends Control
 @onready var Objects = $ObjArray
 
 # Обработка нажатия кнопки создания нового счета
-func _on_add_sections_button_down() -> void: pass
+func _on_add_sections_button_down() -> void: Global.emit_signal("open_window", Global.Pages.SECTION)
 
 # Обработка нажатия кнопки создания движения средств
 func _on_cash_flow_button_down() -> void:
