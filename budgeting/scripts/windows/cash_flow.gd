dd20424d03cb20348bbe4923d66b46a748395b68
Yulya-S <yulya_Shabsnova_03@mail.ru>
2025-08-01 02:10:16 +0800

    changing the partition type

--- budgeting/scripts/windows/cash_flow.gd
+++ budgeting/scripts/windows/cash_flow.gd
@@ -17,6 +17,7 @@ func _extra_errors() -> bool:
 func set_extra(extra_idx: int = 0) -> void:
 	Extra.selected = extra_idx
 	if Request.select(second_table, "income", "id="+str(Global.get_OB_id(Extra)))[0].income: ConsumptionIncome.set_text("Доход")
+	else: ConsumptionIncome.set_text("Расход")
 
 # Изменение значение счета после проведения транзакции
 func _update_wallet_value(delete: bool = false) -> void:
