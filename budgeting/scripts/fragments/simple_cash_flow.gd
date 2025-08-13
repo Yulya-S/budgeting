8bc5f59e54770add20117e14aa1facfda05cbe64
Yulya-S <yulya_Shabsnova_03@mail.ru>
2025-08-13 16:32:23 +0800

    Target Account Arrow

--- budgeting/scripts/fragments/simple_cash_flow.gd
+++ budgeting/scripts/fragments/simple_cash_flow.gd
@@ -5,9 +5,14 @@ extends PageFragment
 @onready var Value = $Value
 @onready var Date = $Date
 
+var wallet_ids: Array = [0, 0]
+var section_id: int = 0
+
 # Изменение значений
 func set_values(data: Dictionary) -> void:
 	id = data.id
+	wallet_ids = [data.wallet_id, data.wallet_2_id]
+	section_id = section_id
 	Title.set_text(str(data.title))
 	var wallet_title: String = str(data.wallet_title)
 	var wallet2_title: String = ""
@@ -17,7 +22,7 @@ func set_values(data: Dictionary) -> void:
 		3:
 			wallet2_title = wallet_title
 			wallet_title = Request.select(Request.Tables.LOANS, "title", "id="+str(data.wallet_2_id))[0].title
-	if wallet2_title != "": wallet2_title = "-> " + wallet2_title
+	if wallet2_title != "": Wallet2Title.get_child(0).visible = true
 	WalletTitle.set_text(wallet_title)
 	Wallet2Title.set_text(wallet2_title)
 	Value.set_text(str(data.value))
