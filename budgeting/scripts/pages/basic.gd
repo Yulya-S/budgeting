d6280c0ce602dfb60d942f81cc93967181d9df20
Yulya-S <yulya_Shabsnova_03@mail.ru>
2025-08-29 23:46:56 +0800

    Changing the Home Page

--- budgeting/scripts/pages/basic.gd
+++ budgeting/scripts/pages/basic.gd
@@ -2,11 +2,9 @@ extends Control
 # Подключение путей к объектам в сцене
 @onready var Budget = $Menu/Budget
 @onready var CashFlow = $Menu/CashFlow
-@onready var ObjectsSection = $ScrollContainer/VBoxContainer/Sections/ObjArray
 
 # Создание главной страницы
 func _ready() -> void:
 	Budget.set_text(str(Request.select_budget()))
 	CashFlow.set_text(str(Request.select_general_wallets_movement()))
-	ObjectsSection.data.where = "s.income = 0 and s.month_limit > 0"
 	Global.emit_signal("update_page")
