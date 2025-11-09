23740b30ce9b4949f5064bf33e8c17007f1ccd62
Yulya-S <yulya_Shabsnova_03@mail.ru>
2025-11-09 09:29:35 +0800

    Account Logout Processing

--- budgeting/scripts/fragments/head.gd
+++ budgeting/scripts/fragments/head.gd
@@ -7,7 +7,7 @@ extends ColorRect
 
 # Изменение положения маркера страницы
 func _ready() -> void:
-	Login.set_text(Global.show_data(Global.config.login))
+	Login.set_text(Global.show_data(File.config.login))
 	Marker.position.x = (5 * (Global.current_page + 3)) + (39.68 * (Global.current_page + 2)) - 1
 	Date.set_text(Global.dictionary_date_to_str(Global.date).split(" ")[0])
 	DayEnd.start((60 - Global.date.second) + (60 * (60 - Global.date.minute)) + (60 * 60 * (24 - Global.date.hour)))
@@ -41,3 +41,8 @@ func _on_timer_timeout() -> void:
 	DayEnd.start(60 * 60 * 24)
 	Global.date = Time.get_datetime_dict_from_system()
 	Date.set_text(Global.date)
+
+
+func _on_exit_button_down() -> void:
+	File.clear_config()
+	Global.emit_signal("open_new_page", Global.Pages.REGISTRATION)
