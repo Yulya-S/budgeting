1d29a1837ab7e7bc1d9435e44906b7bcfe749ab9
Yulya-S <yulya_Shabsnova_03@mail.ru>
2025-11-09 08:12:31 +0800

    Correction of comments

--- budgeting/scripts/pages/settings.gd
+++ budgeting/scripts/pages/settings.gd
@@ -17,8 +17,9 @@ extends ColorRect
 # Стартовое изменение страницы настроек
 func _ready() -> void:
 	File.load_lang(Language)
-	var data: Dictionary = Request.select(Request.Tables.SETTINGS)[0]
+	var data: Dictionary = Request.select(Request.Tables.SETTINGS)[0] # Получение настроек
 	EventType.button_pressed = bool(data.event_page_calendar)
+	# Настройка выбора цвета
 	for i in Colors.get_children():
 		if i is ColorPickerButton:
 			var picker = i.get_picker()
@@ -31,9 +32,6 @@ func _ready() -> void:
 	DarkTheme.button_pressed = bool(data.dark_theme)
 	if data.color_preset: _on_color_scheme_cus_item_selected(data.color_scheme)
 	else: _on_color_scheme_pre_item_selected(data.color_scheme)
-	
-			
-	#_on_color_scheme_pre_item_selected(ColorSchemePre.selected)
 
 # Изменение цветов в примере отображения
 func changed_color():
@@ -89,14 +87,11 @@ func _on_event_type_toggled(toggled_on: bool) -> void: File.set_CB(EventType)
 func _on_preinstalled_toggled(toggled_on: bool) -> void:
 	Preinstalled.button_pressed = toggled_on
 	File.set_CB(Preinstalled)
+	# Изменение отображения параметров оформления
 	ColorSchemeCus.visible = toggled_on
 	ColorSchemePre.visible = not toggled_on
-	if toggled_on:
-		show_colors()
-		_on_color_scheme_cus_item_selected(ColorSchemeCus.selected)
-	else:
-		hide_colors()
-		_on_color_scheme_pre_item_selected(ColorSchemePre.selected)
+	if toggled_on: _on_color_scheme_cus_item_selected(ColorSchemeCus.selected)
+	else: _on_color_scheme_pre_item_selected(ColorSchemePre.selected)
 	changed_color()
 	
 # Обработка изменения светлой и тёмной темы
@@ -105,7 +100,7 @@ func _on_dark_theme_toggled(_toggled_on: bool) -> void:
 	if not Preinstalled.button_pressed: _on_color_scheme_pre_item_selected(ColorSchemePre.selected)
 	else: changed_color()
 
-# Обработка выбра количества цветов
+# Обработка выбора количества цветов
 func _on_color_scheme_cus_item_selected(index: int) -> void:
 	ColorSchemeCus.selected = index
 	show_colors()
@@ -113,15 +108,16 @@ func _on_color_scheme_cus_item_selected(index: int) -> void:
 
 # Стандартные цветовые схемы
 func _on_color_scheme_pre_item_selected(index: int) -> void:
+	hide_colors()
 	ColorSchemePre.selected = index
 	ColorSchemeCus.selected = 1
 	match index:
-		0: _change_theme("3a9891", "c8c8c8", "3aa49c", "414141")
-		2: _change_theme("aa76c6", "dfdf62", "6b6316", "52306a")
-		3: _change_theme("ad5252", "808080", "813333", "3b3b3b")
-		4: _change_theme("df8662", "72c8a3", "8f4e33", "2d5d57")
-		5: _change_theme("e198ae", "b9e198", "801938", "44622b")
-		_:
+		0: _change_theme("3a9891", "c8c8c8", "3aa49c", "414141") # Стандартная
+		2: _change_theme("aa76c6", "dfdf62", "6b6316", "52306a") # Лимон со смородиной
+		3: _change_theme("ad5252", "808080", "813333", "3b3b3b") # Ржавый металл
+		4: _change_theme("df8662", "72c8a3", "8f4e33", "2d5d57") # Лиса на поляне
+		5: _change_theme("e198ae", "b9e198", "801938", "44622b") # Ягода на ветке
+		_: # Серая
 			ColorSchemeCus.selected = 0
 			color1.color = Color("#484848")
 	changed_color()
@@ -132,8 +128,7 @@ func _on_close_button_down() -> void:
 	get_parent().remove_child(self)
 
 # Обработка нажатия кнопки удаления пользователя
-func _on_delete_button_down() -> void:
-	$ConfirmationDialog.visible = true
+func _on_delete_button_down() -> void: $ConfirmationDialog.visible = true
 
 # Обработка подтверждения удаления пользователя
 func _on_confirmation_dialog_confirmed() -> void:
@@ -142,6 +137,7 @@ func _on_confirmation_dialog_confirmed() -> void:
 
 # Обработка нажатия кнопки сохранения настроек
 func _on_apply_button_down() -> void:
+	# Получение данных со страницы
 	var values: Array = []
 	for i in get_children():
 		if not i.visible: continue
@@ -151,4 +147,6 @@ func _on_apply_button_down() -> void:
 			"Control": for l in i.get_children(): values.append('"'+l.color.to_html()+'"')
 	values.pop_front()
 	values.append('"'+Time.get_date_string_from_system()+'"')
+	# Сохранение записи в базе данных
 	Request.update_record(Request.Tables.SETTINGS, 1, values)
+	_on_close_button_down()
