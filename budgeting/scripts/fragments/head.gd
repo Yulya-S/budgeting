78088ca508cf15febe1c7d783202feb330875df7
Yulya-S <yulya_Shabsnova_03@mail.ru>
2025-11-11 16:13:06 +0800

    Changing comments

--- budgeting/scripts/fragments/head.gd
+++ budgeting/scripts/fragments/head.gd
@@ -8,7 +8,14 @@ func _ready() -> void:
 	$Login.set_text(File.show_data(File.config.login))
 	$Marker.position.x = (5 * (Global.current_page + 3)) + (39.68 * (Global.current_page + 2)) - 1
 	Date.set_text(Global.dictionary_date_to_str(Global.date).split(" ")[0])
+	# Запуск таймера с учетом остатка времени до конца дня
 	DayEnd.start((60 - Global.date.second) + (60 * (60 - Global.date.minute)) + (60 * 60 * (24 - Global.date.hour)))
+
+# Обработка окончания работы таймера
+func _on_timer_timeout() -> void:
+	DayEnd.start(60 * 60 * 24)
+	Global.date = Time.get_datetime_dict_from_system()
+	Date.set_text(Global.date)
 
 # Обработка нажатия кнопки настройки 
 func _on_setting_button_down() -> void: Global.emit_signal("open_window", Global.Pages.SETTINGS, null, Global.Dirs.PAGES)
@@ -31,14 +38,8 @@ func _on_loan_button_down() -> void: Global.emit_signal("open_new_page", Global.
 # Обработка нажатия кнопки событий 
 func _on_event_button_down() -> void: Global.emit_signal("open_new_page", Global.Pages.EVENT)
 
-# Обработка нажатия кнопки отчетов 
+# Обработка нажатия кнопки отчетов
 func _on_report_button_down() -> void: pass
-
-# Обработка окончания работы таймера
-func _on_timer_timeout() -> void:
-	DayEnd.start(60 * 60 * 24)
-	Global.date = Time.get_datetime_dict_from_system()
-	Date.set_text(Global.date)
 
 # Обработка нажатия кнопки выхода из аккаунта
 func _on_exit_button_down() -> void:
