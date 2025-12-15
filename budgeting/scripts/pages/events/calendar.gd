c301a6592ce3b2bfd3c729e9b781c52d1ebe7fb3
Yulya-S <yulya_Shabsnova_03@mail.ru>
2025-12-15 09:52:59 +0800

    Populate the event list when hovering over calendar cells

--- budgeting/scripts/pages/events/calendar.gd
+++ budgeting/scripts/pages/events/calendar.gd
@@ -16,26 +16,7 @@ var event_days: Array = []
 var day_count: int = 30
 var date: Dictionary = {}
 
-func data_update(new_filter: ColorRect) -> void:
-	if not filter: filter = new_filter
-	
-	for i in [Cells, Legend]: for l in i.get_children():
-		l.queue_free()
-		i.remove_child(l)
-	
-	Legend.add_child(legend_element_path.instantiate())
-	
-	var data_filter: Dictionary = filter.get_filter()
-	Request.create_multiplied_events_table(data_filter.date)
-	date = Time.get_datetime_dict_from_datetime_string(data_filter.date, true)
-	if date.weekday == 0: date.weekday = 7
-	day_count = Request.select_day_count(data_filter.date)	
-	event_days = Request._select_event_days()
-	_update_legend()
-	
-func _update_legend(idx: int = 0) -> void:
-	if not idx: idx = Global.date.day
-	change_list = Request.select_multiplied_events_list(filter.get_filter().date, str(idx))
+var select_cell: int = 0
 
 func _process(delta: float) -> void:
 	if len(change_list) > 0: lines.append(Request.match_update_list_element(Request.ObjectVariants.EVENT, change_list.pop_front(), self))
@@ -49,39 +30,35 @@ func _process(delta: float) -> void:
 		var value: Dictionary = event_days.pop_front()
 		Cells.get_child(int(value.date.split("-")[-1]) + date.weekday - 2).add_event()
 
+func data_update(new_filter: ColorRect) -> void:
+	for i in Cells.get_children():
+		i.queue_free()
+		Cells.remove_child(i)
+	
+	if not filter: filter = new_filter
+	var data_filter: Dictionary = filter.get_filter()
+	Request.create_multiplied_events_table(data_filter.date)
+	date = Time.get_datetime_dict_from_datetime_string(data_filter.date, true)
+	if date.weekday == 0: date.weekday = 7
+	day_count = Request.select_day_count(data_filter.date)	
+	event_days = Request._select_event_days()
+	_update_legend()
+	
+func _update_legend(idx: int = 0) -> void:
+	for i in Legend.get_children():
+		i.queue_free()
+		Legend.remove_child(i)
+	
+	Legend.add_child(legend_element_path.instantiate())
+	
+	if not idx: idx = Global.date.day
+	change_list = Request.select_multiplied_events_list(filter.get_filter().date, str(idx))
 
-## Изменение параметров запроса
-#func set_data(_where: String = "", new_date: String = "", _order: String = "") -> void:
-	#if new_date != "":
-		#date = new_date
-		#day_count = Request.select_day_count(date)
-	#if _where == "" and new_date == "" and _order == "": return
-	#update_page()
-	#
-## Заполнение страницы
-#func update_page(close_page: String = "") -> void:
-	## Очистка страницы
-	#for i in [Calendar, Legend]: for l in i.get_children():
-		#l.queue_free()
-		#i.remove_child(l)
-	## Создание первой строки легенды
-	#Legend.add_child(legend_element_path.instantiate())
-	#Legend.get_child(-1).color = Color.html("#dfdfdf")
-	## Получение данных
-	#events_color = {}
-	#lines = Request.select_events(date)
-	#for i in lines: if i.event_id not in events_color.keys():
-		#events_color[i.event_id] = "#ffffff"
-		#legend_objects.append(i)
-		#legend_objects[-1].id = i.event_id
-	#for i in range(len(events_color.keys())):
-		#events_color[events_color.keys()[i]] = ColorScheme.get_color(events_color.keys()[i]-1, Request.select(Request.Tables.EVENTS, "COUNT(id)-1 count")[0].count)
-		#legend_objects[i].color = events_color[events_color.keys()[i]]
-	## Обновление страницы родителя
-	#if get_parent().get("update_page"):	get_parent().update_page(close_page)
-#
-## Выделение событий цветом
-#func mark_event(id: int) -> void: for i in Calendar.get_children(): i.mark_event(id)
-#
-## Снятие выделения с события
-#func deselect_event(id: int, color: Color) -> void: for i in Calendar.get_children(): i.deselect_event(id, color)
+func set_cell(index: String) -> void:
+	select_cell = int(index)
+	_update_legend(select_cell)
+
+func reset_cell(index: String) -> void:
+	if int(index) != select_cell: return
+	select_cell = 0
+	_update_legend()
--- budgeting/scripts/pages/events/cell.gd
+++ budgeting/scripts/pages/events/cell.gd
@@ -1,4 +1,5 @@
 extends ColorRect
+@onready var Parent = $"../../"
 
 var lines: Array = []
 
@@ -13,17 +14,6 @@ func set_values(idx: int, current_month: bool, day_count: int) -> void:
 		$Completed.visible = true
 		$Completed.modulate = ColorScheme.get_color(95, 100)
 	elif Global.date.day == idx + 1: color = ColorScheme.get_color(3, 6, ColorScheme.system_gradient)
-	
-	
-	#Number.set_text(str(index))
-	#if today: Background.color = Color.html("#f7cdcd")
-	#else: Background.color = Color.html("#ffffff")
-	#Completed.visible = complete
-	#for i in range(len(get_parent().get_parent().lines)):
-		#if int(get_parent().get_parent().lines[0].date.split("-")[-1]) != index: break
-		#Objects.add_child(event_path.instantiate())
-		#Objects.get_child(-1).set_object(get_parent().get_parent().lines[0], get_parent().get_parent().events_color)
-		#get_parent().get_parent().lines.pop_front()
 
 func add_event(): $Marker.visible = true
 	
@@ -33,3 +23,8 @@ func add_event(): $Marker.visible = true
 #
 ## Снятие выделения с события
 #func deselect_event(id: int, color: Color) -> void: for i in Objects.get_children(): if i.id == id: i.color = color
+
+
+func _on_mouse_entered() -> void: Parent.set_cell($Label.text)
+
+func _on_mouse_exited() -> void: Parent.reset_cell($Label.text)
