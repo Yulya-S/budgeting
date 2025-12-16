cfa4afce8b43185356eb58b89a283e069f1bd2a3
Yulya-S <yulya_Shabsnova_03@mail.ru>
2025-12-17 07:36:29 +0800

    marking of calendar days off

--- budgeting/scripts/pages/events/cell.gd
+++ budgeting/scripts/pages/events/cell.gd
@@ -8,8 +8,9 @@ func _ready() -> void: ColorScheme.repainting(self)
 # Изменение номера дня
 func set_values(idx: int, current_month: bool, next_month: bool, day_count: int) -> void:
 	if idx >= 0 and idx < day_count: $Label.set_text(str(idx+1))
-	else: color = ColorScheme.get_color(6, 6, ColorScheme.system_gradient)
-	if not next_month or Global.date.day > idx + 1 or day_count <= idx:
+	elif not $Label.text: color = ColorScheme.get_color(6, 6, ColorScheme.system_gradient)
+	if $Label.text and get_parent().get_child_count() % 7 in [0, 6]: color = ColorScheme.get_color(4.7, 6, ColorScheme.system_gradient)
+	if not next_month or Global.date.day > idx + 1 or day_count <= idx or not $Label.text:
 		$Completed.visible = true
 		$Completed.modulate = ColorScheme.get_color(95, 100)
 	elif current_month and Global.date.day == idx + 1: color = ColorScheme.get_color(3, 6, ColorScheme.system_gradient)
