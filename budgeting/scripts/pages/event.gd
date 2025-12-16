234e97a4b925990920abcef96feecb9742936b82
Yulya-S <yulya_Shabsnova_03@mail.ru>
2025-12-16 09:37:20 +0800

    Update event.gd

--- budgeting/scripts/pages/event.gd
+++ budgeting/scripts/pages/event.gd
@@ -1,5 +1,4 @@
 extends Page
-
 # Подгружаемыу объекты
 var objArray = load("res://scenes/fragments/obj_array.tscn")
 var calendar = load("res://scenes/pages/events/calendar.tscn")
@@ -14,12 +13,12 @@ func _update_page() -> void:
 	remove_child(get_child(-1))
 	# Создание нового формата отображения событий
 	if not Request.select(Request.Tables.SETTINGS)[0].event_page_calendar:
-		add_child(objArray.instantiate())
-		get_child(-1).size = Vector2(1152, 473)
-		get_child(-1).position = Vector2(0, 170)
-	else:
 		add_child(calendar.instantiate())
 		get_child(-1).size = Vector2(456, 473)
+		get_child(-1).position = Vector2(0, 170)
+	else:
+		add_child(objArray.instantiate())
+		get_child(-1).size = Vector2(1152, 473)
 		get_child(-1).position = Vector2(0, 170)
 	Objects = get_child(-1) # Получение пути к списку событий
 	update_data() # Обновлениие данных в списке событий
