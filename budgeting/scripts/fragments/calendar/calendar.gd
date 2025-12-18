b34177f1ec0ff28f344b46453f0f4ec1dc8475a4
Yulya-S <yulya_Shabsnova_03@mail.ru>
2025-12-18 08:28:45 +0800

    creating a calendar class

---
+++ budgeting/scripts/fragments/calendar/calendar.gd
@@ -0,0 +1,30 @@
+extends Node
+class_name Calendar
+# Подключение путей к объектам в сцене
+@onready var Cells = $Cells
+
+# Переменные
+@export var cell_path: Resource = load("res://scenes/pages/events/cell.tscn") # Путь к сцене ячеек календаря
+var date: Dictionary = {} # Сохранение выбранной даты
+var day_count: int = 30 # Количество дней в выбранном месяце
+
+# Постепенное создание элементов страницы
+func _process(_delta: float) -> void:
+	if _end_create():
+		Cells.add_child(cell_path.instantiate())
+		Cells.get_child(-1).set_values(Cells.get_child_count() - date.weekday, Global.date_comparison(Global.date, date, "==", false),
+			Global.date_comparison(Global.date, date, "=<", false), day_count)
+
+# Проверка завершено ли создание ячеек календаря
+func _end_create() -> bool: return Cells.get_child_count() < day_count + date.weekday - 1 or Cells.get_child_count() % 7 != 0
+
+# Обновление данных
+func data_update(new_filter: ColorRect) -> void:
+	# Очистка календаря
+	for i in Cells.get_children():
+		i.queue_free()
+		Cells.remove_child(i)
+	# Получение новых данных для создания на странице
+	date = Global.date_to_dict(new_filter.get_filter().date)
+	if date.weekday == 0: date.weekday = 7 # Смена индекса дня недели
+	day_count = Request.select_day_count(Global.date_to_str(date))	
--- budgeting/scripts/pages/events/calendar.gd
+++ budgeting/scripts/pages/events/calendar.gd
@@ -1,51 +1,35 @@
-extends Control
+extends Calendar
 # Подключение путей к объектам в сцене
-@onready var Cells = $Cells
 @onready var SelectedCell = $SelectedCell
 @onready var Legend = $Legend/VBoxContainer
 
 # Переменные
 # Ссылки
 var legend_element_path: Resource = load("res://scenes/fragments/list_elements/event_legend.tscn") # Путь к сцене элемента легенды
-var cell_path: Resource = load("res://scenes/pages/events/cell.tscn") # Путь к сцене ячеек календаря
 # Списки
 var lines: Array = [] # Список объектов для создания на странице
 var change_list: Array = [] # Список для изменения объектов на странице
 var event_days: Array = [] # Список дат для маркировки в каллендаре
 # Обновляемые
-var day_count: int = 30 # Количество дней в выбранном месяце
-var date: Dictionary = {} # Сохранение выбранной даты
 var select_cell: int = 0 # Индекс выбранной ячейки календаря
-var filter: Dictionary = {} # Сохранение данные фильтра
 
-func _ready() -> void: ColorScheme.repainting(SelectedCell)
 
 # Постепенное создание элементов страницы
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
+	super._process(delta)
 	if len(change_list) > 0: lines.append(Request.match_update_list_element(Request.ObjectVariants.EVENT, change_list.pop_front(), self))
 	if len(lines) > 0:
 		Legend.add_child(legend_element_path.instantiate())
 		Legend.get_child(-1).set_values(lines.pop_front())
-	if Cells.get_child_count() < day_count + date.weekday - 1 or Cells.get_child_count() % 7 != 0:
-		Cells.add_child(cell_path.instantiate())
-		Cells.get_child(-1).set_values(Cells.get_child_count() - date.weekday, Global.date_comparison(Global.date, date, "==", false),
-			Global.date_comparison(Global.date, date, "=<", false), day_count)
-	elif len(event_days) > 0:
+	if not _end_create() and len(event_days) > 0:
 		var value: Dictionary = event_days.pop_front()
 		Cells.get_child(int(value.date.split("-")[-1]) + date.weekday - 2).add_event()
 
 # Обновление данных
 func data_update(new_filter: ColorRect) -> void:
-	# Очистка календаря
-	for i in Cells.get_children():
-		i.queue_free()
-		Cells.remove_child(i)
-	# Получение новых данных для создания на странице
-	filter = new_filter.get_filter()
-	Request.create_multiplied_events_table(filter.date)
-	date = Global.date_to_dict(filter.date)
-	if date.weekday == 0: date.weekday = 7 # Смена индекса дня недели
-	day_count = Request.select_day_count(filter.date)	
+	super.data_update(new_filter)
+	# Получение новых данных для создания заполнения легенды
+	Request.create_multiplied_events_table(Global.date_to_str(date))
 	event_days = Request._select_event_days()
 	_update_legend(select_cell)
 
@@ -56,19 +40,18 @@ func _update_legend(idx: int = 0) -> void:
 		i.queue_free()
 		Legend.remove_child(i)
 	Legend.add_child(legend_element_path.instantiate()) # Создание заголовка
-	var filter_date: Dictionary = Global.date_to_dict(filter.date)
 	# Изменение индекса выбранной ячейки если она отсутствует
 	if not idx:
 		idx = Global.date.day
-		if not Global.date_comparison(filter_date, Global.date, "==", false): idx = 1
+		if not Global.date_comparison(date, Global.date, "==", false): idx = 1
 	change_list = Request.select_multiplied_events_list(str(idx))
 	# Изменение расположения маркера выбранной даты
 	# Получение позиции по горизонтали
-	var weekday: int = (filter_date.weekday - 1 + idx) % 7 - 1
+	var weekday: int = (date.weekday - 1 + idx) % 7 - 1
 	if weekday < 0: weekday = 6
 	SelectedCell.position.x = (78 * weekday) + 45 - (2 * (weekday - 1))
 	# Получение позиции по вертикали
-	var week_number: int = int((filter_date.weekday - 1 + idx) / 7 - 1)
+	var week_number: int = int((date.weekday - 1 + idx) / 7 - 1)
 	if weekday == 6: week_number -= 1
 	SelectedCell.position.y = (78 * week_number) + 80 - (2 * (week_number - 2))
 
