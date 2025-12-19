d70b29ec2e8e64062e30056abec4de5c29974716
Yulya-S <yulya_Shabsnova_03@mail.ru>
2025-12-19 17:51:01 +0800

    removing an unused script

--- budgeting/scripts/pages/basic.gd
+++ budgeting/scripts/pages/basic.gd
@@ -3,6 +3,13 @@ extends Control
 @onready var Budget = $Menu/Budget
 @onready var CashFlow = $Menu/CashFlow
 @onready var Objects = $ScrollContainer/VBoxContainer
+@onready var Cells = $ScrollContainer/VBoxContainer/Events/Calendar
+
+# Переменные
+var cell_path: Resource = load("res://scenes/pages/events/cell.tscn") # Путь к сцене ячеек календаря
+var event_days: Array = [] # Список дат для маркировки в каллендаре
+var date: Dictionary = {} # Сохранение выбранной даты
+var day_count: int = 30 # Количество дней в выбранном месяце
 
 # Создание главной страницы
 func _ready() -> void:
@@ -13,6 +20,15 @@ func _ready() -> void:
 	#Budget.set_text(str(Request.select_budget()))
 	#CashFlow.set_text(str(Request.select_general_wallets_movement()))
 	#Global.emit_signal("update_page")
+
+# Постепенное создание элементов страницы
+func _process(delta: float) -> void:
+	if Cells.get_child_count() < 14:
+		Cells.add_child(cell_path.instantiate())
+		Cells.get_child(-1).set_values(date.day + Cells.get_child_count() - date.weekday - 1, true, true, day_count)
+	elif len(event_days) > 0:
+		var value: int = int(event_days.pop_front().date.split("-")[-1])
+		if value <= date.day - date.weekday + 14: Cells.get_child(value - (date.day - date.weekday + 1)).add_event()
 	
 # Запуск обновления данных на странице
 func _update_page() -> void:
@@ -23,10 +39,23 @@ func _update_page() -> void:
 	#update_data()
 	
 # Обновление данных
-func update_data() -> void: _find_objects(Objects)
+func update_data() -> void:
+	_find_objects(Objects)
+	# Создание ячеек календаря
+	# Очистка календаря
+	for i in Cells.get_children():
+		i.queue_free()
+		Cells.remove_child(i)
+	# Получение новых данных для создания на странице
+	date = Global.date_to_dict()
+	if date.weekday == 0: date.weekday = 7 # Смена индекса дня недели
+	day_count = Request.select_day_count(Global.date_to_str())
+	# Получение новых данных для создания заполнения легенды
+	Request.create_multiplied_events_table("-".join([date.year, date.month, "01"]))
+	event_days = Request.select_event_days("CAST(strftime('%d', date) AS INTEGER)>"+str(date.day-date.weekday))
 
 # Поиск и запуск изменения списков и графиков
 func _find_objects(obj: Variant) -> void:
-	if obj.name in ["ObjArray", "DailyTransactions", "Calendar"]: obj.data_update()
+	if obj.name in ["ObjArray", "DailyTransactions"]: obj.data_update()
 	for i in obj.get_children(): _find_objects(i)
 	
