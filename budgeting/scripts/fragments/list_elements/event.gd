c7578052fe3b3b262d1ab683e8046929887c477a
Yulya-S <yulya_Shabsnova_03@mail.ru>
2026-01-10 02:30:30 +0800

    dimensions of the past event marker

--- budgeting/scripts/fragments/list_elements/event.gd
+++ budgeting/scripts/fragments/list_elements/event.gd
@@ -2,11 +2,6 @@ extends Fragment
 # Подключение пути к объекту в сцене
 @onready var Completed = $Completed
 
-# Изменение размера маркера завершения
-func _ready() -> void:
-	super._ready()
-	Completed.custom_minimum_size = custom_minimum_size
-
 # Изменение значений
 func set_values(data: Dictionary) -> void:
 	_event_values(data, "__ET" + str(data.event_type))
@@ -14,3 +9,4 @@ func set_values(data: Dictionary) -> void:
 	# Изменение видимости маркера завершения
 	Completed.modulate = color
 	Completed.visible = data.completed
+	Completed.size = custom_minimum_size
