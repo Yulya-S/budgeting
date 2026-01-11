4f7c20e748b834dd648832e5811ee09762c7328e
Yulya-S <yulya_Shabsnova_03@mail.ru>
2026-01-11 19:37:55 +0800

    Changing the code for list items

--- budgeting/scripts/fragments/list_elements/section.gd
+++ budgeting/scripts/fragments/list_elements/section.gd
@@ -6,11 +6,7 @@ var m_index: int = 0 # Индекс объекта для изменения ц�
 
 # Изменение значений
 func set_values(data: Dictionary) -> void:
-	$ConsumptionIncome.set_text("" if data.id <= 4 else "__CI" + str(data.income))
 	super.set_values(data)
-	$Progress.visible = not data.income and data.month_limit > 0
-	$Marker.size[1] = custom_minimum_size[1]
-	if data.month_limit <= 0 or data.income: $Month_Limit.set_text("")
 	m_index = get_parent().get_child_count() - 2
 	
 # Обработка наведения мыши на контейнер
