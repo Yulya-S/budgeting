ea99a62dfa79db820265e3e9228460f5b6e8bea2
Yulya-S <yulya_Shabsnova_03@mail.ru>
2026-02-03 14:09:57 +0800

    checking creation of information page

--- budgeting/scripts/main.gd
+++ budgeting/scripts/main.gd
@@ -33,9 +33,22 @@ func _notification(what: int) -> void: if Request.db: if what == Window.NOTIFICA
 # Открытие страницы
 func _open_window(page: Global.Pages, id: Variant = null,
 	dir: Global.Dirs = Global.Dirs.WINDOWS, parent: Variant = null) -> void:
-	if get_child_count() > 0 and "Inf" in get_child(-1).name and Global.enum_key(Global.Pages, page).replace("_", "") == get_child(-1).name.to_lower(): return
 	add_child(load("res://scenes/"+Global.enum_key(Global.Dirs, dir)+"/"+Global.enum_key(Global.Pages, page)+".tscn").instantiate())
-	if id: Global.run_func(get_child(-1), "set_object", [id, parent])
+	if get_child_count() > 1 and _check_inf_page(): Global.delete_child(self, get_child(-1))
+	#elif id: Global.run_func(get_child(-1), "set_object", [id, parent])
+
+# Проверка имени крайней страницы
+func _check_inf_page() -> bool:
+	if not _ch_name("@", -2) and not _ch_name("Inf", -2) and not _ch_name(): return false
+	if _ch_par("page_type") and _ch_par(): return true
+	return false
+
+# Проверка фрагмена названия дочернего элемента
+func _ch_name(text: String = "@", idx: int = -1) -> bool: return text in get_child(idx).name
+
+# Проверка равенства параметров двух дочерних элементов
+func _ch_par(param_name: String = "idx", idx_1: int = -1, idx_2: int = -2) -> bool:
+	return get_child(idx_1).get(param_name) == get_child(idx_2).get(param_name)
 
 # Очистка экрана и открытие новой страницы
 func _open_new_page(page: Global.Pages, id: Variant = null, parent: Variant = null) -> void:
