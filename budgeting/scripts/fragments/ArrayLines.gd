05fc78b3fd8f1d488780623c490b6408d7bf41b4
Yulya-S <yulya_Shabsnova_03@mail.ru>
2026-02-19 15:23:10 +0800

    Fixing a calendar bug

--- budgeting/scripts/fragments/ArrayLines.gd
+++ budgeting/scripts/fragments/ArrayLines.gd
@@ -16,6 +16,7 @@ func add_obj(parent: Variant, ObjectVariant: Request.ObjectVariants, requesting:
 # Очистка списка
 func clear(values: Array, parent: Variant) -> void:
 	Global.clear_scene(parent)
-	if parent.get_parent().get("first_line"): parent.add_child(path.instantiate())
+	if parent.get_parent().get("first_line") or parent.get_parent().name == "Events":
+		parent.add_child(path.instantiate())
 	change_list = values
 	lines = []
