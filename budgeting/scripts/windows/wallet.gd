ef3c1ac1c49b8dd67caac8de2caa274140c95140
Yulya-S <yulya_Shabsnova_03@mail.ru>
2026-03-03 08:41:49 +0800

    removing unnecessary script files

--- budgeting/scripts/windows/wallet.gd
+++
@@ -1,8 +0,0 @@
-extends CreationWindow
-# Подключение пути к объектам в 
-@onready var Title = $Title
-	
-# Проверка введенных данных
-func check_object() -> bool:
-	super.check_object()
-	return _set_error(Request.select(table, "id", 'title="'+Title.get_text()+'"'))
