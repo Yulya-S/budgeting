1b2c875a8f5fd75775809988a6dbe510de0fa804
Yulya-S <yulya_Shabsnova_03@mail.ru>
2026-03-10 14:17:28 +0800

    cleaning fragments/head file code

--- budgeting/scripts/fragments/head.gd
+++ budgeting/scripts/fragments/head.gd
@@ -17,39 +17,9 @@ func update_date() -> void:
 func _on_exit_button_down() -> void:
 	File.clear_config()
 	Request.connection_user_db()
-	Global.emit_signal("open_new_page", Global.Pages.REGISTRATION)
-
-# Обработка нажатия кнопки подсказок
-func _on_hints_button_down() -> void: Global.emit_signal("open_window", Global.Pages.HINTS, null, Global.Dirs.PAGES)
-
-# Обработка нажатия кнопки настройки 
-func _on_setting_button_down() -> void: Global.emit_signal("open_window", Global.Pages.SETTINGS, null, Global.Dirs.PAGES)
-
-# Обработка нажатия кнопки очистки данных
-func _on_cleaning_button_down() -> void: Global.emit_signal("open_window", Global.Pages.CLEANING, null, Global.Dirs.PAGES)
-
-# Обработка нажатия кнопки главная 
-func _on_main_button_down() -> void: Global.emit_signal("open_new_page", Global.Pages.BASIC)
-
-# Обработка нажатия кнопки кошелька 
-func _on_wallet_button_down() -> void: Global.emit_signal("open_new_page", Global.Pages.WALLET)
-
-# Обработка нажатия кнопки разделов
-func _on_section_button_down() -> void: Global.emit_signal("open_new_page", Global.Pages.SECTION)
-
-# Обработка нажатия кнопки движения средств 
-func _on_flow_button_down() -> void: Global.emit_signal("open_new_page", Global.Pages.CASH_FLOW)
-
-# Обработка нажатия кнопки кредита
-func _on_loan_button_down() -> void: Global.emit_signal("open_new_page", Global.Pages.LOAN)
-
-# Обработка нажатия кнопки событий 
-func _on_event_button_down() -> void: Global.emit_signal("open_new_page", Global.Pages.EVENT)
-
-# Обработка нажатия кнопки отчетов
-func _on_report_button_down() -> void: Global.emit_signal("open_new_page", Global.Pages.REPORT)
-
-# Открытия окошка уведомлений
+	_emit_onp(Global.Pages.REGISTRATION)
+	
+# Открытия окна уведомлений
 func _on_notification_button_down() -> void:
 	NotificationObjects.update_data()
 	Request.update_notifications_new()
@@ -60,3 +30,30 @@ func _on_notification_button_down() -> void:
 func _on_notification_cleaning_button_down() -> void:
 	Request.clear_notifications()
 	NotificationObjects.update_data()
+
+# Вызов сигнала открытия окна поверх текущего
+func _emit_ow(new_page: Global.Pages) -> void: Global.emit_signal("open_window", new_page, null, Global.Dirs.PAGES)
+
+# Вызов сигнала открытия нового окна
+func _emit_onp(new_page: Global.Pages) -> void: Global.emit_signal("open_new_page", new_page)
+
+# Обработка перехода по страницам
+func _on_hints_button_down() -> void: _emit_ow(Global.Pages.HINTS) # Подсказки
+
+func _on_setting_button_down() -> void: _emit_ow(Global.Pages.SETTINGS) # Настройки
+
+func _on_cleaning_button_down() -> void: _emit_ow(Global.Pages.CLEANING) # Очистка данных
+
+func _on_main_button_down() -> void: _emit_onp(Global.Pages.BASIC) # Главная
+
+func _on_wallet_button_down() -> void: _emit_onp(Global.Pages.WALLET) # Кошельки
+
+func _on_section_button_down() -> void: _emit_onp(Global.Pages.SECTION) # Разделы
+
+func _on_flow_button_down() -> void: _emit_onp(Global.Pages.CASH_FLOW) # Движения средств
+
+func _on_loan_button_down() -> void: _emit_onp(Global.Pages.LOAN) # Займы
+
+func _on_event_button_down() -> void: _emit_onp(Global.Pages.EVENT) # События
+
+func _on_report_button_down() -> void: _emit_onp(Global.Pages.REPORT) # Отчеты
