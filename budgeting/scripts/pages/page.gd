d65e6102edbc64a2e28ae97faf97ecbdc9be60e3
Yulya-S <yulya_Shabsnova_03@mail.ru>
2026-03-11 10:31:00 +0800

    Moving code from section to page

--- budgeting/scripts/pages/page.gd
+++ budgeting/scripts/pages/page.gd
@@ -99,3 +99,19 @@ func _on_Loan_add_interest_button_down() -> void:
 # Обработка нажатия кнопки погашения займа
 func _on_Loan_add_payment_button_down() -> void:
 	if Request.select_possibility_opening_payment(): Global.emit_signal("open_window", Global.Pages.PAYMENT)
+
+# Разделы
+# Применение выделений секций на круговой диаграмме
+func highlighting_graph_sections(idx: int, set_highlighting: bool = true) -> void:
+	if set_highlighting: $PieChart.set_highlighter(idx)
+	else: $PieChart.reset_highlighter(idx)
+
+# Обработка нажатия кнопки создания нового счета
+func _on_Section_add_section_button_down() -> void: Global.emit_signal("open_window", Global.Pages.SECTION)
+
+# Обработка нажатия кнопки создания движения средств
+func _on_Section_cash_flow_button_down() -> void:
+	if Request.select_possibility_opening_cashFlow(): Global.emit_signal("open_window", Global.Pages.CASH_FLOW)
+
+# Обработка нажатия кнопки создания подраздела
+func _on_Section_add_subsection_button_down() -> void: Global.emit_signal("open_window", Global.Pages.SUBSECTION)
