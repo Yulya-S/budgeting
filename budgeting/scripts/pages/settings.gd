c97da8adbc60154690d3e14fc1f19deb6996ac42
Yulya-S <yulya_Shabsnova_03@mail.ru>
2026-03-11 16:12:26 +0800

    Cleaning up pages/settings code

--- budgeting/scripts/pages/settings.gd
+++ budgeting/scripts/pages/settings.gd
@@ -2,9 +2,9 @@ extends PageWindow
 # Подключение путей к объектам в сцене
 # Параметры
 @onready var Language = $Language
-@onready var EventType = $EventType
-@onready var Preinstalled = $Preinstalled
-@onready var DarkTheme = $DarkTheme
+@onready var EventType = $Event_Page_Calendar
+@onready var Preinstalled = $Color_Preset
+@onready var DarkTheme = $Dark_Theme
 @onready var ColorSchemePre = $ColorSchemePre
 @onready var ColorSchemeCus = $ColorSchemeCus
 # Цвета
@@ -15,20 +15,21 @@ extends PageWindow
 func _ready() -> void:
 	File.load_lang(Language)
 	var data: Dictionary = Request.select(Request.Tables.SETTINGS)[0] # Получение настроек
-	EventType.button_pressed = bool(data.event_page_calendar)
-	# Настройка выбора цвета
-	for i in Colors.get_children():
-		if i is ColorPickerButton:
-			var picker: ColorPicker = i.get_picker()
-			picker.picker_shape = ColorPicker.SHAPE_VHS_CIRCLE
-			picker.color_modes_visible = false
-			picker.sliders_visible = false
-			picker.presets_visible = false
-			i.color = Color("#"+data[Global.lower(i)])
-	_on_preinstalled_toggled(bool(data.color_preset))
-	DarkTheme.button_pressed = bool(data.dark_theme)
-	if data.color_preset: _on_color_scheme_cus_item_selected(data.color_scheme)
-	else: _on_color_scheme_pre_item_selected(data.color_scheme)
+	for i in get_children():
+		match i.get_class():
+			"CheckButton":
+				i.button_pressed = bool(data[SF.l(i)])
+				call("_on_"+SF.l(i)+"_toggled", i.button_pressed)
+			"OptionButton":
+				call("_on_"+str("cus" if data.color_preset else "pre"), data.color_scheme)
+			"Control":
+				for l in i.get_children():
+					var picker: ColorPicker = l.get_picker()
+					picker.picker_shape = ColorPicker.SHAPE_VHS_CIRCLE
+					picker.color_modes_visible = false
+					picker.sliders_visible = false
+					picker.presets_visible = false
+					l.color = Color("#"+data[SF.l(l)])
 
 # Изменение цветов в примере
 func changed_color() -> void:
@@ -51,80 +52,63 @@ func hide_colors() -> void: for i in Colors.get_children(): i.visible = false
 func show_colors() -> void:
 	hide_colors()
 	for i in range(ColorSchemeCus.selected + 1): Colors.get_child(i).visible = true
-	
-# Изменение цвета - контраст
-func _contrast(c1: String, c2: String) -> void:
-	color1.color = Color("#" + c1)
-	$Colors/Color_2.color = Color("#" + c2)
-	
-# Изменение цвета - триада
-func _triad(c1: String, c2: String, c3: String) -> void:
-	color1.color = Color("#" + c1)
-	$Colors/Color_2.color = Color("#" + c2)
-	$Colors/Color_3.color = Color("#" + c3)
-
-# Смена темы между светлой и тёмной
-func _change_theme(c1_l: String, c2_l: String, c1_d: String, c2_d: String) -> void:
-	if DarkTheme.button_pressed: _contrast(c1_d, c2_d)
-	else: _contrast(c1_l, c2_l)
 
 # Изменение цветов
-func _on_color_1_color_changed(_color: Color) -> void: changed_color()
+func _on_color_1_changed(_color: Color) -> void: changed_color()
 
-func _on_color_2_color_changed(_color: Color) -> void: changed_color()
+func _on_color_2_changed(_color: Color) -> void: changed_color()
 
-func _on_color_3_color_changed(_color: Color) -> void: changed_color()
+func _on_color_3_changed(_color: Color) -> void: changed_color()
 
-func _on_color_4_color_changed(_color: Color) -> void: changed_color()
+func _on_color_4_changed(_color: Color) -> void: changed_color()
 
 # Обработка изменения языка приложения
 func _on_language_item_selected(_index: int) -> void: File.read_lang(Language)
 
 # Обработка изменения способа отображения календаря
-func _on_event_type_toggled(_toggled_on: bool) -> void: File.set_CB(EventType)
+func _on_event_page_calendar_toggled(_toggled_on: bool) -> void: File.set_CB(EventType)
 
 # Обработка изменения темы оформления между предустановленной и персонализированной
-func _on_preinstalled_toggled(toggled_on: bool) -> void:
-	Preinstalled.button_pressed = toggled_on
+func _on_color_preset_toggled(toggled_on: bool) -> void:
 	File.set_CB(Preinstalled)
-	# Изменение отображения параметров оформления
 	ColorSchemeCus.visible = toggled_on
 	ColorSchemePre.visible = not toggled_on
-	if toggled_on: _on_color_scheme_cus_item_selected(ColorSchemeCus.selected)
-	else: _on_color_scheme_pre_item_selected(ColorSchemePre.selected)
+	call("_on_"+str("cus" if toggled_on else "pre"))
 	changed_color()
 	
 # Обработка изменения светлой и тёмной темы
 func _on_dark_theme_toggled(_toggled_on: bool) -> void:
 	File.set_CB(DarkTheme)
-	if not Preinstalled.button_pressed: _on_color_scheme_pre_item_selected(ColorSchemePre.selected)
-	else: changed_color()
+	if Preinstalled.button_pressed: changed_color()
+	else: _on_pre()
+
+# Изменение цвета
+func _set_colors(color_values: Array) -> void:
+	ColorSchemeCus.selected = len(color_values) - 1
+	for i in range(len(color_values)): Colors.get_child(i).color = Color("#" + color_values[i])
+
+# Смена темы между светлой и тёмной
+func _change_theme(light: Array, dark: Array) -> void: _set_colors(dark if DarkTheme.button_pressed else light)
 
 # Обработка выбора количества цветов
-func _on_color_scheme_cus_item_selected(index: int) -> void:
+func _on_cus(index: int = ColorSchemeCus.selected) -> void:
 	ColorSchemeCus.selected = index
 	show_colors()
 	changed_color()
 
 # Стандартные цветовые схемы
-func _on_color_scheme_pre_item_selected(index: int) -> void:
+func _on_pre(index: int = ColorSchemePre.selected) -> void:
 	hide_colors()
 	ColorSchemePre.selected = index
-	ColorSchemeCus.selected = 1
 	match index:
-		0: _change_theme("3a9891", "c8c8c8", "3aa49c", "414141") # Стандартная
-		2: _change_theme("aa76c6", "dfdf62", "6b6316", "52306a") # Лимон со смородиной
-		3: _change_theme("ad5252", "808080", "813333", "3b3b3b") # Ржавый металл
-		4: _change_theme("df8662", "72c8a3", "8f4e33", "2d5d57") # Лиса на поляне
-		5: _change_theme("e198ae", "b9e198", "801938", "44622b") # Ягода на ветке
-		6: _change_theme("981475", "3b9fc8", "ab3c96", "2c3498") # Ежевика
-		7: # Пингвин
-			ColorSchemeCus.selected = 2
-			if DarkTheme.button_pressed: _triad("9c9c98", "ab5732", "474745")
-			else: _triad("474745", "9c9c98", "ffe1ca")
-		_: # Серая
-			ColorSchemeCus.selected = 0
-			color1.color = Color("#636363")
+		0: _change_theme(["3a9891", "c8c8c8"], ["3aa49c", "414141"]) # Стандартная
+		2: _change_theme(["aa76c6", "dfdf62"], ["6b6316", "52306a"]) # Лимон со смородиной
+		3: _change_theme(["ad5252", "808080"], ["813333", "3b3b3b"]) # Ржавый металл
+		4: _change_theme(["df8662", "72c8a3"], ["8f4e33", "2d5d57"]) # Лиса на поляне
+		5: _change_theme(["e198ae", "b9e198"], ["801938", "44622b"]) # Ягода на ветке
+		6: _change_theme(["981475", "3b9fc8"], ["ab3c96", "2c3498"]) # Ежевика
+		7: _change_theme(["474745", "9c9c98", "ffe1ca"], ["9c9c98", "ab5732", "474745"]) # Пингвин
+		_: _set_colors(["636363"]) # Серая
 	changed_color()
 
 # Обработка нажатия кнопки удаления пользователя
@@ -133,7 +117,7 @@ func _on_delete_button_down() -> void: $ConfirmationDialog.visible = true
 # Обработка подтверждения удаления пользователя
 func _on_confirmation_dialog_confirmed() -> void:
 	Request.delete_user()
-	Global.emit_signal("open_new_page", Global.Pages.REGISTRATION)
+	SF.op_np(Global.Pages.REGISTRATION)
 
 # Обработка нажатия кнопки сохранения настроек
 func _on_apply_button_down() -> void:
